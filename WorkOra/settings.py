2fa5dfd7a8ab7f84bfc77433872ea8e551aafc03
abhii314 <abhijeetkumar954615@gmail.com>
2025-06-08 05:41:01 +0530

    Add www to non-www redirect middleware

--- WorkOra/settings.py
+++ WorkOra/settings.py
@@ -30,9 +30,6 @@ DEBUG = False
 ALLOWED_HOSTS = [ os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost')]
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
 
 # Application definition
 
@@ -50,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'home.middleware.WwwRedirectMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = 'login_page'
